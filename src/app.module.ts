43f5f3521e36e76167fc0c072da95d27ba4a27db
Nutty[Dev] <anan@myorder.ai>
2025-12-04 16:18:16 +0700

    base structure of the project

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BooksService } from './books/books.service';
+import { BooksModule } from './books/books.module';
+import { CartsController } from './carts/carts.controller';
+import { CartsModule } from './carts/carts.module';
+import { OrdersService } from './orders/orders.service';
+import { OrdersModule } from './orders/orders.module';
+
+@Module({
+  imports: [BooksModule, CartsModule, OrdersModule],
+  controllers: [AppController, CartsController],
+  providers: [AppService, BooksService, OrdersService],
+})
+export class AppModule {}
